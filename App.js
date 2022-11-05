f498e57108edd6d0dbfcec416a213e22739485ab
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2022-11-05 14:21:44 +0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title={'Button'}>Click Me</Button>
       <StatusBar style="auto" />
     </View>
   );
